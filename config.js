d63e8f0cbd38f398f2a715aa4d68d58a5ee88edc
titusnguyen111 <huudat890@gmail.com>
2021-11-19 10:49:35 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'chó phúc ',
+    introDesc: `hãy trả lời câu hỏi dưới đây `,
+    btnIntro: 'oke',
+    title: 'cậu nợ tui 1 bữa ăn ',
+    desc: 'hãy chọn 1 trong 2 thứ dưới đây :> ',
+    btnYes: 'beef wellington <33',
+    btnNo: 'hủ tíu ',
+    question:'mày có bị ngu không ',
+    btnReply: 'ok ',
+    reply: 'đúng t rất ngu  ',
+    mess: 'Tớ biết mà 🥰',
+    messDesc: 'còn chờ gì nữa mà ko chốt đơn quán ăn ',
+    btnAccept: 'Okiiiii lun <3',
+messLink: 'http://fb.com' //link mess của các bạn. 
+}
+
+    
